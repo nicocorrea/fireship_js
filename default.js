285191485d353b005551281e113ce15fd64c730a
Nicolas Correa <correan10@gmail.com>
2022-12-23 09:27:32 -0300

    destructuring

--- default.js
+++ default.js
@@ -16,3 +16,22 @@ Object.getPrototypeOf(dog) === animal; // true
 Object.getPrototypeOf(animal) === Object.prototype; // true
 
 Object.getPrototypeOf(Object.prototype) === null; // true
+
+/* OBJECT DESTRUCTURING */
+
+const person = {
+  name: "John",
+  age: 32,
+  city: "New York",
+  country: "USA",
+};
+
+const { name, age } = person;
+
+// Object destructuring with alias
+
+const { name: firstName, age: years } = person;
+
+// Array destructuring
+const fruits = ["apple", "banana", "orange"];
+const [first, second, third] = fruits;
