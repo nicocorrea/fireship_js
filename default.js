fc1b8e887fbed3250076bc1091ec74eeca4bcfae
Nicolas Correa <correan10@gmail.com>
2022-12-24 15:12:08 -0300

    spread

--- default.js
+++ default.js
@@ -1,5 +1,5 @@
-/* Remember that PROTOTYPE works in the form of a chain/tree to real instances of the object,
- and when changing those from the parent, it changes those then referenced by the child object */
+// ? Remember that PROTOTYPE works in the form of a chain/tree to real instances of the object,
+// ? and when changing those from the parent, it changes those then referenced by the child object.
 
 const animal = {
   dna: "ATCG",
@@ -17,7 +17,7 @@ Object.getPrototypeOf(animal) === Object.prototype; // true
 
 Object.getPrototypeOf(Object.prototype) === null; // true
 
-/* OBJECT DESTRUCTURING */
+// ? OBJECT DESTRUCTURING
 
 const person = {
   name: "John",
@@ -35,3 +35,28 @@ const { name: firstName, age: years } = person;
 // Array destructuring
 const fruits = ["apple", "banana", "orange"];
 const [first, second, third] = fruits;
+
+// ? The SPREAD syntax/operator used to combine objects/arrays
+
+const obj1 = {
+  name: "Nico",
+  age: 36,
+};
+
+const obj2 = {
+  age: 40,
+  hasCar: true,
+};
+
+const combined_option1 = Object.assign({}, obj1, obj2);
+const combined_option2 = { ...obj1, ...obj2 };
+
+console.log(combined_option1);
+console.log(combined_option2);
+
+const list_1 = [1, 2, 3, 4];
+const list_2 = [3, 4, 5, 6];
+
+const combined_list = [...list_1, ...list_2];
+
+console.log(combined_list);
