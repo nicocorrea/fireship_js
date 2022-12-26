407f5ccf7ad14cae45bd9da850dee0f1d38e9dd3
Nicolas Correa <correan10@gmail.com>
2022-12-25 21:42:47 -0300

    cumulative sum

---
+++ cumulative_sum.js
@@ -0,0 +1,13 @@
+const arr = [1, 3, 5, 7];
+
+// OPTION 1
+function cumulative(arr) {
+  let total = 0;
+  arr.forEach((x) => (total += x));
+  return total;
+}
+
+console.log(cumulative(arr));
+
+// OPTION 2, using Reduce
+console.log(arr.reduce((acc, cur) => acc + cur));
